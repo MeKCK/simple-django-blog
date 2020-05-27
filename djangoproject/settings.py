5bd4812992dde1b8de2d3062e128048e655e670e
kiprono <42340517+MeKCK@users.noreply.github.com>
2020-05-27 22:14:40 +0300

    posts

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'poats',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'djangoproject',
+        'USER' : 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
